e0a9ac512737cbab46524e95e00e940996ac083f
Festanny <60522731+Festanny@users.noreply.github.com>
2022-09-06 09:41:38 +0400

    upd

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
   $('.first-slider').slick();
+
+  $( window ).resize(function() {
+    mlSlider();
+  });
+
+  function mlSlider() {
+    if ($(window).width() > 1200) {
+      $cnt = 1170;
+    } else {
+      $cnt = 570;
+    }
+    $cntMl = ($(window).width() - $cnt) / 2 + 15;
+    $('.slick-arrow').css('margin-left', $cntMl);
+  }
+  mlSlider();
+
 });
+
